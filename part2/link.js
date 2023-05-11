42c1f916fb357a94b74a106cd345e341c3fdab00
HImanshu Ray <himanshuray@HImanshus-MacBook-Pro.local>
2023-05-11 19:18:23 +0530

    changes updated

---
+++ part2/link.js
@@ -0,0 +1,11 @@
+console.dir(document);
+
+//select element by by id
+const  mainheading=document.getElementById("main-heading");
+//ye object return karta hai
+
+
+
+//select element by query selector
+console.log(document.querySelectorAll("#main-heading"));
+console.log(document.querySelectorAll(".nav-item"));
---
+++ part2/link2.js
@@ -0,0 +1,7 @@
+
+const mainhead = document.getElementById("main-heading");
+console.log(mainhead.innerText);
+
+// change the styles of Elements 
+const mainheading = document.querySelector("#main-heading");
+console.log(mainheading.style)
