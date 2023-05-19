208a3792b2be605166318e0a76fbb936cbb8ac55
HImanshu Ray <himanshuray@HImanshus-MacBook-Pro.local>
2023-05-19 14:32:52 +0530

    js

--- part2/link4.js
+++ part2/link4.js
@@ -2,3 +2,22 @@
 const link = document.querySelector("a");
 console.log(link.getAttribute('href').slice(1));
 link.setAttribute("href", "https://google.com");
+
+let navitem = document.getElementsByTagName("a"); //returns a html collection of all the elements with class name nav -Item
+console.log(navitem);
+
+//lopping in html tags
+
+for (let i = 0; i < navitem.length; i++) {
+
+    const single = navitem[i];
+    console.log(single);
+    single.style.backgroundColor = "#fffe";
+    single.style.color = "red";
+}
+
+//htmlcollection to array;
+navitem = Array.from(navitem);
+navitem.forEach((single) => {
+    single.style.fontWeight = "bold";
+});
